2d4cd1d25fb15b258b0c3829c7db998ffb46dba0
Hallekh <lekhnovska.halyna@gmail.com>
2022-09-10 21:03:24 +0200

    adding project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [pages, setPages] = useState([]);
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://api.github.com/users/Hallekh?client_id=b95f3e88bef940623650&client_secret=b9e273cd5c98c6f81fb5a8331d942a50485680f3&sort=created')
+    .then((res) => {
+      setPages(res.data);
+    })
+    .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/Hallekh/repos')
+    .then((repositories) => setRepositories(repositories.data))
+    .catch((err) => console.log(err));
+  }); 
+
+  const photo = pages.avatar_url
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='box1'>
+        <div className='img'>
+          <img src={photo} alt="photo" />
+        </div>
+        <div className='info'>
+          <p>FullName: {pages.name}</p>
+          <p>UserName: {pages.login}</p>
+          <p>Location: {pages.location}</p>
+          <p>email: {pages.email}</p>
+        </div>
+      </div>
+      <div className='box2'>
+        <h3>Repositories</h3>
+        <ul>
+          {repositories.map((item) => {
+            return <li key={item.id}><a href={'https://github.com/' + item.full_name}>{item.name}</a></li>
+          })}
+        </ul>
+
+      </div>
     </div>
   );
 }
